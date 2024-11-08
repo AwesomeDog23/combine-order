2c5812efddb5c26c107886a9627f801b86e18aad
Cooper <cooper.m@taylorjoelle.com>
2024-11-08 12:40:10 -0700

    feat: add priceSet with shopMoney to order creation

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -329,6 +329,12 @@ export const action = async ({ request }) => {
           variantId: item.variantId,
           quantity: item.quantity,
           requiresShipping: true,
+          priceSet: {
+            shopMoney: {
+              amount: "0.00",
+              currencyCode: "USD",
+            }
+          },
         }));
 
         const regularOrderCreateResponse = await admin.graphql(
